7793355fc9680266684bf75bde0decd8d06e8f7d
laruwu <larakanevsky@gmail.com>
2023-06-21 19:47:29 -0300

    mas cambios

--- MudleServer/app.js
+++ MudleServer/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const http = require('http');
+
 
 const config = require('./config')
 const Either = require('./models/Either')
@@ -10,13 +10,13 @@ const AuthenticationLogic = require('./logics/AuthenticationLogic')
 const ImportarLogic = require('./logics/ImportarLogic')
 const ItemsLogic = require('./logics/ItemsLogic')
 var jsonwebtoken = require("jsonwebtoken");
-const Utils = require("./models/Utils");
-const MongoDBDAO = require("./DAOs/MongoDBDAO");
+const Utils = require('./models/Utils')
+const MongoDBDAO = require('./DAOs/MongoDBDAO')
 const app = express();
 const port = config.serverPort;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());       
+app.use(express.urlencoded({extended: true})); 
 
 // app.get('/usuarios',async (req, res) => {
 //   console.log(req.query)
@@ -24,196 +24,126 @@ app.use(express.urlencoded({ extended: true }));
 //   res.send(result)
 // })
 
-app.post("/login", async function (request, response) {
+app.post('/login',async function(request, response){
   var mail = request.body.mail;
   var password = request.body.password;
-  if (!mail || !password) {
-    response.status(400).send("Bad request");
+  if(!mail || !password) {
+      response.status(400).send("Bad request");
   } else {
-    let userFound = await AuthenticationLogic.userCredentialsAreValid(
-      mail,
-      password
-    );
-    if (userFound.isRight()) {
-      var claim = {
-        id: userFound.getRight()._id,
-        tipoUsuario: userFound.getRight().tipoUsuario,
-      };
-      var token = jsonwebtoken.sign(claim, config.jwtSecret, {
-        expiresIn: config.jwtExpiresInSec,
-      });
-      response.send("JWT: " + token);
-    } else {
-      return response.status(401).end("Mail o contraseña incorrectos");
-    }
+    let userFound = await AuthenticationLogic.userCredentialsAreValid(mail, password);
+      if(userFound.isRight()) {
+          var claim = {
+              id: userFound.getRight()._id,
+              tipoUsuario: userFound.getRight().tipoUsuario,
+          }
+          var token = jsonwebtoken.sign(claim, config.jwtSecret, {
+              expiresIn: config.jwtExpiresInSec
+          });
+          response.send("JWT: " + token);
+      } else {
+          return response.status(401).end("Mail o contraseña incorrectos");
+      }
   }
 });
 
 var apiProtectedRouter = express.Router();
-apiProtectedRouter.use(function (request, response, next) {
-  var token = request.get("Authorization");
-  if (token && token.includes("Bearer")) {
-    token = token.replace("Bearer ", "");
-    jsonwebtoken.verify(
-      token.trim(),
-      config.jwtSecret,
-      function (error, decoded) {
-        if (error) {
-          console.log("jwt error: " + error);
-          response.status(401).send("Invalid Token. Error Message: " + error);
-        } else {
-          console.log("decoded", decoded);
-          request.decodedToken = decoded;
-          return next();
-        }
-      }
-    );
-  } else {
-    response.status(401).send("Necesita un token.");
-  }
+apiProtectedRouter.use(function(request, response, next){
+    var token = request.get("Authorization");
+    if(token && token.includes("Bearer")) {
+        token = token.replace("Bearer ","");
+        jsonwebtoken.verify(token.trim(), config.jwtSecret, function(error, decoded){
+            if(error) {
+                console.log("jwt error: " + error);
+                response.status(401).send("Invalid Token. Error Message: " + error);
+            } else {
+                console.log("decoded", decoded)
+                request.decodedToken = decoded;
+                return next();
+            }
+        });
+    } else {
+        response.status(401).send("Necesita un token.");
+    }
 });
 
 app.use("/protected", apiProtectedRouter);
 
-app.post("/usuarios", async (req, res) => {
+app.post('/usuarios',async (req, res) => {
   let result = await new UsuariosLogic().DAO.insert(req.body);
   let serverResponse = Utils.eitherServerResponseToUserResponse(result);
   res.send(serverResponse.status,serverResponse.translateToUser());
 })
 
-apiProtectedRouter.post('/usuarios/moodleCredentials',async (req, res) => {
-  const userLogic= new UsuariosLogic();
-  let userResponse = await userLogic.getUser(req.decodedToken.id)
-
-let username = req.body.username;
-
-let password = req.body.password;
-
-  let user = userResponse.getRight();
-  user.moodleCredentials = {username:username,password:password}
-  let result = await userLogic.DAO.update(req.decodedToken.id,user);
-  //let serverResponse = Utils.eitherServerResponseToUserResponse(result);
-  //res.send(serverResponse.status,serverResponse.translateToUser());
-  res.send(result.getRight())
-})
-
 apiProtectedRouter.post('/items',async (req, res) => {
 
-apiProtectedRouter.post("/items", async (req, res) => {
-  let result = await new ItemsLogic().insertNewItem(
-    req.decodedToken.id,
-    req.body,
-    "duenio"
-  );
+  let result = await new ItemsLogic().insertNewItem(req.decodedToken.id,req.body,"duenio");
   let serverResponse = Utils.eitherServerResponseToUserResponse(result);
   res.status(serverResponse.status).send(serverResponse.translateToUser());
   //res.send(result);
-});
+})
 
-apiProtectedRouter.post("/items/compartir", async (req, res) => {
-  console.log("BODI:", req.body);
+apiProtectedRouter.post('/items/compartir',async (req, res) => {
+  console.log("BODI:",req.body)
   const idItem = req.body.idItem;
   const tipoPermiso = req.body.tipoPermiso;
   const arrayMailsUsuarios = req.body.arrayMailsUsuarios;
-  if (!idItem || !tipoPermiso || !arrayMailsUsuarios) {
-    res
-      .status(400)
-      .send(
-        "Los campos son: idItem, tipoPermiso ('read','read-write','duenio'), arrayMailsUsuarios. Alguno esta faltando."
-      );
+  if(!idItem || !tipoPermiso || !arrayMailsUsuarios){
+    res.status(400).send("Los campos son: idItem, tipoPermiso ('read','read-write','duenio'), arrayMailsUsuarios. Alguno esta faltando.")
   }
 
-  let result = await new ItemsLogic().compartirItemConMuchos(
-    idItem,
-    tipoPermiso,
-    arrayMailsUsuarios
-  );
+  let result = await new ItemsLogic().compartirItemConMuchos(idItem,tipoPermiso,arrayMailsUsuarios);
   //let serverResponse = Utils.eitherServerResponseToUserResponse(result);
   //res.send(serverResponse.status,serverResponse.translateToUser());
   res.send(result);
-});
+})
 
-apiProtectedRouter.get("/items", async (req, res) => {
-  console.log(req.body);
-  console.log("dec token", req.decodedToken);
+apiProtectedRouter.get('/items',async (req, res) => {
+  console.log(req.body)
+  console.log("dec token",req.decodedToken)
   let result = await new ItemsLogic().getUserItems(req.decodedToken.id);
   let serverResponse = Utils.eitherServerResponseToUserResponse(result);
-  console.log("uguuu", serverResponse);
+  console.log("uguuu",serverResponse)
   // res.send(serverResponse.status,serverResponse.translateToUser());
   res.send(serverResponse.content);
-});
+})
 
-apiProtectedRouter.post("/importFromMoodle", async (request, response) => {
-  console.log(request.body);
-  console.log("dec token", request.decodedToken);
+apiProtectedRouter.post('/importFromMoodle',async (request, response) => {
+  console.log(request.body)
+  console.log("dec token",request.decodedToken)
 
-  let usuarioResponse = await new UsuariosLogic().getUser(request.decodedToken.id);
-  if(usuarioResponse.isLeft()){
-    return usuarioResponse;
-  }
-  let usuario = usuarioResponse.getRight();
-  if(usuario.moodleCredentials==null){
-return Either.left("El usuario no tiene cargadas las credenciales de Moodle.")
-  }
-  console.log("mod cred",usuario.moodleCredentials)
-
-// let importResult =await new ImportarLogic().importarDeMoodle(request.decodedToken.id)
-// console.log("importResult",importResult)
-// response.send(importResult)
-
-const postData = JSON.stringify({
-  username:usuario.moodleCredentials.username,
-  password:usuario.moodleCredentials.password
-});
-
-const options = {
-  hostname: 'localhost',
-  port: 3001,
-  path: '/items',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(postData)
-  }
-};
-  const req = http.request(options, (res) => {
-    let respuesta = '';
+let importResult =await new ImportarLogic().importarDeMoodle()
+console.log("importResult",importResult)
   
-    res.on('data', (chunk) => {
-      respuesta += chunk;
-    });
+  // const req = http.request(options, (res) => {
+  //   let respuesta = '';
   
-    res.on('end',async () => {
-      console.log(`Response: ${respuesta}`);
+  //   res.on('data', (chunk) => {
+  //     respuesta += chunk;
+  //   });
+  
+  //   res.on('end', () => {
+  //     console.log(`Response: ${respuesta}`);
+  //     response.send(JSON.parse(respuesta));
+  //   });
+  // });
+  
+  // req.on('error', (error) => {
+  //   console.error(`Request error: ${error}`);
+  // });
+  
+  // req.write(postData);
+  // req.end();
 
-      let items = await new ImportarLogic().parseAPIItems(JSON.parse(respuesta),request.decodedToken.id);
-      for (let index = 0; index < items.length; index++) {
-        const element = items[index];
-        new ItemsLogic().insertNewItem(request.decodedToken.id,element,"duenio")
-      }
-      //new ItemsLogic().insertNewItem(request.decodedToken.id,)
-      //new UsuariosLogic.addItemToUser(request.decodedToken.id,idItem,permisoItem)
-
-      response.send(items);
-    });
-  });
-  
-  req.on('error', (error) => {
-    console.error(`Request error: ${error}`);
-  });
-  
-  req.write(postData);
-  req.end();
 
   // let result = await new ItemsLogic().getUserItems(req.decodedToken.id);
   // let serverResponse = Utils.eitherServerResponseToUserResponse(result);
   // console.log("uguuu",serverResponse)
   // // res.send(serverResponse.status,serverResponse.translateToUser());
   // res.send(serverResponse.content);
-});
+})
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-module.exports = app;
+  console.log(`Example app listening on port ${port}`)
+})
